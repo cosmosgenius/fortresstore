28a2b485065fa9a523fb7583e716713b8b3edae2
Sharat M R <cosmosgenius@gmail.com>
2017-06-15 00:25:39 +0530

    Add screenshot and developer to admin

--- core/admin.py
+++ core/admin.py
@@ -1,10 +1,21 @@
 from django.contrib import admin
-from core.models import App
+from core.models import App, Developer, Screenshot
 
 
 class AppAdmin(admin.ModelAdmin):
-    list_display = ["app_id", "name"]
+    list_display = ["app_id", "name", "url"]
     search_fields = ["app_id", "name"]
 
 
+class DeveloperAdmin(admin.ModelAdmin):
+    list_display = ["email", "name", "url"]
+    search_fields = ["email", "name"]
+
+
+class ScreenshotAdmin(admin.ModelAdmin):
+    list_display = ["app", "url"]
+
+
 admin.site.register(App, AppAdmin)
+admin.site.register(Developer, DeveloperAdmin)
+admin.site.register(Screenshot, ScreenshotAdmin)
