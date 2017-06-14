aec2c5eee008f42eeda7d95bf49ce5ec2757b799
Sharat M R <cosmosgenius@gmail.com>
2017-06-14 15:27:43 +0530

    Add app model

--- core/models.py
+++ core/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from taggit.managers import TaggableManager
 
-# Create your models here.
+
+class App(models.Model):
+    app_id = models.CharField(max_length=500)
+    name = models.CharField(max_length=500)
+    description = models.TextField()
+    developer_name = models.CharField(max_length=500)
+    developer_email = models.EmailField(null=True)
+    developer_url = models.URLField()
+    price = models.DecimalField(decimal_places=4, max_digits=10)
+    rating = models.DecimalField(decimal_places=2, max_digits=5)
+    cover_large = models.ImageField()
+    cover_small = models.ImageField()
+    url = models.URLField()
+
+    tags = TaggableManager()
